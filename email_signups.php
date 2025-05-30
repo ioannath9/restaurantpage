43081e8ace5a6b22dd30432eff2179952b7d01f2
ioannath9 <140355861+ioannath9@users.noreply.github.com>
2025-05-30 21:48:06 +0200

    Add files via upload

---
+++ email_signups.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Birdman restaurant</title>
+<link rel="icon" href="LOGO.png" type="image/png">
+</head>
+
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "This is the last email that signed up : " .$_POST["email"];
+?>
+
+</body>
+</html>
